ff3947ee9fc6a6f7654accec9c91e933aa25b282
kartik3030 <kartik.vij2004@gmail.com>
2026-01-04 10:41:54 +0530

    Backend Fix

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -43,16 +43,15 @@ app.use(
 );
 
 
-
-
 /* ================= AUTH ================= */
 
 const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
 };
+
 
 
 const requireAuth = (req, res, next) => {
